ca1398138c6384e3a2c8d26398794d0e48ae8497
paras966 <66049390+paras966@users.noreply.github.com>
2022-04-15 09:57:03 +0530

    Update hash.py

--- Hash Table/hash.py
+++ Hash Table/hash.py
@@ -1,34 +1,34 @@
-# ## from list
-# stock_prices=[]
-# with open("stock_prices.csv","r") as file:
-#     for line in file:
-#         tokens=line.split(',')
-#         day=tokens[0]
-#         price=float(tokens[1])
-#         stock_prices.append([day,price])
+## from list
+stock_prices=[]
+with open("stock_prices.csv","r") as file:
+    for line in file:
+        tokens=line.split(',')
+        day=tokens[0]
+        price=float(tokens[1])
+        stock_prices.append([day,price])
 
+print(stock_prices)
+
+for element in stock_prices:
+    if element[0]=="7_March":
+        print(element[1])
+
+### BY DICTIONARY
+from turtle import st
+
+
+stock_prices={}
+with open("stock_prices.csv","r") as file:
+    for line in file:
+        tokens=line.split(',')
+        day=tokens[0]
+        price=float(tokens[1])
+        stock_prices[day]=price
 # print(stock_prices)
 
-# for element in stock_prices:
-#     if element[0]=="7_March":
-#         print(element[1])
+print(stock_prices['8_March'])
 
-# ### BY DICTIONARY
-# from turtle import st
-
-
-# stock_prices={}
-# with open("stock_prices.csv","r") as file:
-#     for line in file:
-#         tokens=line.split(',')
-#         day=tokens[0]
-#         price=float(tokens[1])
-#         stock_prices[day]=price
-# # print(stock_prices)
-
-# print(stock_prices['8_March'])
-
-#### Using Hashtable
+### Using Hashtable
 
 class HashTable:
     def __init__(self):
